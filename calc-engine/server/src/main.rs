0e38d094b056afd705f80ee1f6a582ef71e53e57
Your Name <you@example.com>
2022-03-10 14:47:12 +0200

    rs server

--- calc-engine/server/src/main.rs
+++ calc-engine/server/src/main.rs
@@ -1,3 +1,16 @@
-fn main(){
+#[macro_use]
+extern crate rocket;
+use rocket::serde::json::Json;
+use fractal_simulations;
 
+#[get("/mandelbrot?<start_u>&<start_v>&<delta>&<size>&<max_iter>")]
+fn index(start_u: f64, start_v: f64, delta: f64, size: usize, max_iter: u32) -> Json<Vec<u32>> {
+  Json(fractal_simulations::simulate_mandelbrot(
+    start_u, start_v, delta, size, max_iter,
+  ))
+}
+
+#[launch]
+fn rocket() -> _ {
+  rocket::build().mount("/", routes![index])
 }
